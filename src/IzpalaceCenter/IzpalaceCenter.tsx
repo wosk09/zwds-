4ca7c28f6e0a8f4e2a84927c7ad9f07f2b3381f7
wosk09 <75789258+wosk09@users.noreply.github.com>
2025-08-03 14:40:56 +0400

    Update IzpalaceCenter.tsx

--- src/IzpalaceCenter/IzpalaceCenter.tsx
+++ src/IzpalaceCenter/IzpalaceCenter.tsx
@@ -3,7 +3,7 @@ import React, { useCallback, useMemo } from "react";
 import FunctionalAstrolabe from "iztro/lib/astro/FunctionalAstrolabe";
 import "./IzpalaceCenter.css";
 import { Line } from "./Line";
-import { Item } from "./Item"; // ← Вот это добавь
+import Item from "./Item";
 import { fixEarthlyBranchIndex } from "iztro/lib/utils";
 import { Scope } from "iztro/lib/data/types";
 import { IFunctionalHoroscope } from "iztro/lib/astro/FunctionalHoroscope";
