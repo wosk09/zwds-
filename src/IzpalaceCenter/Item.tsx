62906802d831f24d72d941e7c00f67d52b6edb56
wosk09 <75789258+wosk09@users.noreply.github.com>
2025-08-03 16:59:26 +0400

    Update Item.tsx

--- src/IzpalaceCenter/Item.tsx
+++ src/IzpalaceCenter/Item.tsx
@@ -1,12 +1,10 @@
 import React from "react";
 
-// Тип пропсов
 export interface ItemProps {
   title: string;
   content: string | undefined;
 }
 
-// Компонент Item
 const Item = ({ title, content }: ItemProps) => (
   <li>
     <strong>{title}</strong> {content || "N/A"}
